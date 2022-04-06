d8803f88ea913111c83d33b4bd4232454cf29c34
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-04-06 07:36:02 +0000

    picture added

---
+++ dreamerv3-torch/dreamer.py
@@ -0,0 +1,141 @@
+
+import argparse
+import functools
+import os
+import pathlib
+import sys
+
+# os.environ["MUJOCO_GL"] = "osmesa"
+# os.environ["MUJOCO_GL"]="glfw"
+os.environ["MUJOCO_GL"]='egl'
+import numpy as np
+import ruamel.yaml as yaml
+
+sys.path.append(str(pathlib.Path(__file__).parent))
+
+import exploration as expl
+import models
+import tools
+import envs.wrappers as wrappers
+from parallel import Parallel, Damy
+
+import torch
+from torch import nn
+from torch import distributions as torchd
+
+
+to_np = lambda x: x.detach().cpu().numpy()
+
+
+class Dreamer(nn.Module):
+    def __init__(self, obs_space, act_space, config, logger, dataset):
+        super(Dreamer, self).__init__()
+        self._config = config
+        self._logger = logger
+        self._should_log = tools.Every(config.log_every)
+        batch_steps = config.batch_size * config.batch_length
+        self._should_train = tools.Every(batch_steps / config.train_ratio)
+        self._should_pretrain = tools.Once()
+        self._should_reset = tools.Every(config.reset_every)
+        self._should_expl = tools.Until(int(config.expl_until / config.action_repeat))
+        self._metrics = {}
+        # this is update step
+        self._step = logger.step // config.action_repeat
+        self._update_count = 0
+        # Schedules.
+        config.actor_entropy = lambda x=config.actor_entropy: tools.schedule(
+            x, self._step
+        )
+        config.actor_state_entropy = (
+            lambda x=config.actor_state_entropy: tools.schedule(x, self._step)
+        )
+        config.imag_gradient_mix = lambda x=config.imag_gradient_mix: tools.schedule(
+            x, self._step
+        )
+        self._dataset = dataset
+        self._wm = models.WorldModel(obs_space, act_space, self._step, config)
+        self._task_behavior = models.ImagBehavior(
+            config, self._wm, config.behavior_stop_grad
+        )
+        if (
+            config.compile and os.name != "nt"
+        ):  # compilation is not supported on windows
+            self._wm = torch.compile(self._wm)
+            self._task_behavior = torch.compile(self._task_behavior)
+        reward = lambda f, s, a: self._wm.heads["reward"](f).mean()
+        self._expl_behavior = dict(
+            greedy=lambda: self._task_behavior,
+            random=lambda: expl.Random(config, act_space),
+            plan2explore=lambda: expl.Plan2Explore(config, self._wm, reward),
+        )[config.expl_behavior]().to(self._config.device)
+
+    def __call__(self, obs, reset, state=None, training=True):
+        step = self._step
+        if self._should_reset(step):
+            state = None
+        if state is not None and reset.any():
+            mask = 1 - reset
+            for key in state[0].keys():
+                for i in range(state[0][key].shape[0]):
+                    state[0][key][i] *= mask[i]
+            for i in range(len(state[1])):
+                state[1][i] *= mask[i]
+        if training:
+            steps = (
+                self._config.pretrain
+                if self._should_pretrain()
+                else self._should_train(step)
+            )
+            for _ in range(steps):
+                self._train(next(self._dataset))
+                self._update_count += 1
+                self._metrics["update_count"] = self._update_count
+            if self._should_log(step):
+                for name, values in self._metrics.items():
+                    self._logger.scalar(name, float(np.mean(values)))
+                    self._metrics[name] = []
+                if self._config.video_pred_log:
+                    openl = self._wm.video_pred(next(self._dataset))
+                    self._logger.video("train_openl", to_np(openl))
+                self._logger.write(fps=True)
+
+        policy_output, state = self._policy(obs, state, training)
+
+        if training:
+            self._step += len(reset)
+            self._logger.step = self._config.action_repeat * self._step
+        return policy_output, state
+
+    def _policy(self, obs, state, training):
+        if state is None:
+            batch_size = len(obs["image"])
+            latent = self._wm.dynamics.initial(len(obs["image"]))
+            action = torch.zeros((batch_size, self._config.num_actions)).to(
+                self._config.device
+            )
+        else:
+            latent, action = state
+        obs = self._wm.preprocess(obs)
+        embed = self._wm.encoder(obs)
+        latent, _ = self._wm.dynamics.obs_step(
+            latent, action, embed, obs["is_first"], self._config.collect_dyn_sample
+        )
+        if self._config.eval_state_mean:
+            latent["stoch"] = latent["mean"]
+        feat = self._wm.dynamics.get_feat(latent)
+        if not training:
+            actor = self._task_behavior.actor(feat)
+            action = actor.mode()
+        elif self._should_expl(self._step):
+            actor = self._expl_behavior.actor(feat)
+            action = actor.sample()
+        else:
+            actor = self._task_behavior.actor(feat)
+            action = actor.sample()
+        logprob = actor.log_prob(action)
+        latent = {k: v.detach() for k, v in latent.items()}
+        action = action.detach()
+        if self._config.actor_dist == "onehot_gumble":
+            action = torch.one_hot(
+                torch.argmax(action, dim=-1), self._config.num_actions
+            )
