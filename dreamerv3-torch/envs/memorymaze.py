af1d99e92cad194e0b08c4a04558a79a8b0ee236
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-05-03 15:23:05 +0000

    usb: option: Add Telewell TW-LTE 4G

--- dreamerv3-torch/envs/memorymaze.py
+++ dreamerv3-torch/envs/memorymaze.py
@@ -8,4 +8,7 @@ class MemoryMaze:
     def __init__(self, task, obs_key="image", act_key="action", size=(64, 64)):
         if task == "9x9":
             self._env = gym.make("memory_maze:MemoryMaze-9x9-v0")
- 
+        elif task == "15x15":
+            self._env = gym.make("memory_maze:MemoryMaze-15x15-v0")
+        else:
+            raise NotImplemente
