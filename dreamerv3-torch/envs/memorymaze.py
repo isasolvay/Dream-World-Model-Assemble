537a4e4c4a8762c2da130a99b5aa15bb67f97501
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-05-03 15:06:52 +0000

    Add badges to README

--- dreamerv3-torch/envs/memorymaze.py
+++ dreamerv3-torch/envs/memorymaze.py
@@ -5,4 +5,7 @@ import numpy as np
 
 
 class MemoryMaze:
-    def __init__(self, task, obs_ke
+    def __init__(self, task, obs_key="image", act_key="action", size=(64, 64)):
+        if task == "9x9":
+            self._env = gym.make("memory_maze:MemoryMaze-9x9-v0")
+ 
