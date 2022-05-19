88c7bca19e9b7d58abce5b08db9a4d64895872ba
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-05-19 20:28:56 +0000

    1010

--- dreamerv3-torch/envs/wrappers.py
+++ dreamerv3-torch/envs/wrappers.py
@@ -1,3 +1,9 @@
 import datetime
 import gym
-import numpy a
+import numpy as np
+import uuid
+
+
+class TimeLimit(gym.Wrapper):
+    def __init__(self, env, duration):
+        super().__init__(env)
