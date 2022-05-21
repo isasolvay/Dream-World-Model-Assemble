ed7d07a073561d1be0e6f4104a75b99671c01080
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-05-21 17:52:53 +0000

    coomit

--- dreamerv3-torch/envs/wrappers.py
+++ dreamerv3-torch/envs/wrappers.py
@@ -7,3 +7,8 @@ import uuid
 class TimeLimit(gym.Wrapper):
     def __init__(self, env, duration):
         super().__init__(env)
+        self._duration = duration
+        self._step = None
+
+    def step(self, action):
+        assert self._step is not None, 
