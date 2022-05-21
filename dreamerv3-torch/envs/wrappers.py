012ad545650c3251509e82d4829434f5e5fc9c9d
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-05-21 22:44:05 +0000

    desktop

--- dreamerv3-torch/envs/wrappers.py
+++ dreamerv3-torch/envs/wrappers.py
@@ -11,4 +11,7 @@ class TimeLimit(gym.Wrapper):
         self._step = None
 
     def step(self, action):
-        assert self._step is not None, 
+        assert self._step is not None, "Must reset environment."
+        obs, reward, done, info = self.env.step(action)
+        self._step += 1
+        if self._step >= self._duration:
