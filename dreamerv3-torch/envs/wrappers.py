6080072d2c7861a85e331c7e56d27f9bab539222
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-05-23 08:27:56 +0000

    Some

--- dreamerv3-torch/envs/wrappers.py
+++ dreamerv3-torch/envs/wrappers.py
@@ -18,4 +18,9 @@ class TimeLimit(gym.Wrapper):
             done = True
             if "discount" not in info:
                 info["discount"] = np.array(1.0).astype(np.float32)
-     
+            self._step = None
+        return obs, reward, done, info
+
+    def reset(self):
+        self._step = 0
+        return 
