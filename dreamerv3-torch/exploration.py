b102a793948c13ff9d5bdbf901a4077ecc2ff909
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-06-19 12:42:19 +0000

    Update refs

--- dreamerv3-torch/exploration.py
+++ dreamerv3-torch/exploration.py
@@ -8,4 +8,9 @@ import tools
 
 
 class Random(nn.Module):
-    def __init_
+    def __init__(self, config, act_space):
+        super(Random, self).__init__()
+        self._config = config
+        self._act_space = act_space
+
+   
