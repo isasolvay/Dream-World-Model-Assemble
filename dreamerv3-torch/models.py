3425f537967b8187fb8537640500f44f6b72366e
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-07-31 19:34:47 +0000

    Update Danish translation

--- dreamerv3-torch/models.py
+++ dreamerv3-torch/models.py
@@ -45,3 +45,129 @@ class WorldModel(nn.Module):
             config.dyn_input_layers,
             config.dyn_output_layers,
             config.dyn_rec_depth,
+            config.dyn_shared,
+            config.dyn_discrete,
+            config.act,
+            config.norm,
+            config.dyn_mean_act,
+            config.dyn_std_act,
+            config.dyn_temp_post,
+            config.dyn_min_std,
+            config.dyn_cell,
+            config.unimix_ratio,
+            config.initial,
+            config.num_actions,
+            self.embed_size,
+            config.device,
+        )
+        self.heads = nn.ModuleDict()
+        if config.dyn_discrete:
+            feat_size = config.dyn_stoch * config.dyn_discrete + config.dyn_deter
+        else:
+            feat_size = config.dyn_stoch + config.dyn_deter
+        self.heads["decoder"] = networks.MultiDecoder(
+            feat_size, shapes, **config.decoder
+        )
+        if config.reward_head == "symlog_disc":
+            self.heads["reward"] = networks.MLP(
+                feat_size,  # pytorch version
+                (255,),
+                config.reward_layers,
+                config.units,
+                config.act,
+                config.norm,
+                dist=config.reward_head,
+                outscale=0.0,
+                device=config.device,
+            )
+        else:
+            self.heads["reward"] = networks.MLP(
+                feat_size,  # pytorch version
+                [],
+                config.reward_layers,
+                config.units,
+                config.act,
+                config.norm,
+                dist=config.reward_head,
+                outscale=0.0,
+                device=config.device,
+            )
+        self.heads["cont"] = networks.MLP(
+            feat_size,  # pytorch version
+            [],
+            config.cont_layers,
+            config.units,
+            config.act,
+            config.norm,
+            dist="binary",
+            device=config.device,
+        )
+        for name in config.grad_heads:
+            assert name in self.heads, name
+        self._model_opt = tools.Optimizer(
+            "model",
+            self.parameters(),
+            config.model_lr,
+            config.opt_eps,
+            config.grad_clip,
+            config.weight_decay,
+            opt=config.opt,
+            use_amp=self._use_amp,
+        )
+        self._scales = dict(reward=config.reward_scale, cont=config.cont_scale)
+
+    def _train(self, data):
+        # action (batch_size, batch_length, act_dim)
+        # image (batch_size, batch_length, h, w, ch)
+        # reward (batch_size, batch_length)
+        # discount (batch_size, batch_length)
+        data = self.preprocess(data)
+
+        with tools.RequiresGrad(self):
+            with torch.cuda.amp.autocast(self._use_amp):
+                embed = self.encoder(data)
+                post, prior = self.dynamics.observe(
+                    embed, data["action"], data["is_first"]
+                )
+                kl_free = tools.schedule(self._config.kl_free, self._step)
+                dyn_scale = tools.schedule(self._config.dyn_scale, self._step)
+                rep_scale = tools.schedule(self._config.rep_scale, self._step)
+                kl_loss, kl_value, dyn_loss, rep_loss = self.dynamics.kl_loss(
+                    post, prior, kl_free, dyn_scale, rep_scale
+                )
+                preds = {}
+                for name, head in self.heads.items():
+                    grad_head = name in self._config.grad_heads
+                    feat = self.dynamics.get_feat(post)
+                    feat = feat if grad_head else feat.detach()
+                    pred = head(feat)
+                    if type(pred) is dict:
+                        preds.update(pred)
+                    else:
+                        preds[name] = pred
+                losses = {}
+                for name, pred in preds.items():
+                    like = pred.log_prob(data[name])
+                    losses[name] = -torch.mean(like) * self._scales.get(name, 1.0)
+                model_loss = sum(losses.values()) + kl_loss
+            metrics = self._model_opt(model_loss, self.parameters())
+
+        metrics.update({f"{name}_loss": to_np(loss) for name, loss in losses.items()})
+        metrics["kl_free"] = kl_free
+        metrics["dyn_scale"] = dyn_scale
+        metrics["rep_scale"] = rep_scale
+        metrics["dyn_loss"] = to_np(dyn_loss)
+        metrics["rep_loss"] = to_np(rep_loss)
+        metrics["kl"] = to_np(torch.mean(kl_value))
+        with torch.cuda.amp.autocast(self._use_amp):
+            metrics["prior_ent"] = to_np(
+                torch.mean(self.dynamics.get_dist(prior).entropy())
+            )
+            metrics["post_ent"] = to_np(
+                torch.mean(self.dynamics.get_dist(post).entropy())
+            )
+            context = dict(
+                embed=embed,
+                feat=self.dynamics.get_feat(post),
+                kl=kl_value,
+                postent=self.dynamics.get_dist(post).entropy(),
