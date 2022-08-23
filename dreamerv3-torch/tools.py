8a2d7039e380e52bc94110e34cb75e7363b26549
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-08-23 07:24:21 +0000

    changed comment

--- dreamerv3-torch/tools.py
+++ dreamerv3-torch/tools.py
@@ -763,3 +763,139 @@ class Optimizer:
         use_amp=False,
     ):
         assert 0 <= wd < 1
+        assert not clip or 1 <= clip
+        self._name = name
+        self._parameters = parameters
+        self._clip = clip
+        self._wd = wd
+        self._wd_pattern = wd_pattern
+        self._opt = {
+            "adam": lambda: torch.optim.Adam(parameters, lr=lr, eps=eps),
+            "nadam": lambda: NotImplemented(f"{opt} is not implemented"),
+            "adamax": lambda: torch.optim.Adamax(parameters, lr=lr, eps=eps),
+            "sgd": lambda: torch.optim.SGD(parameters, lr=lr),
+            "momentum": lambda: torch.optim.SGD(parameters, lr=lr, momentum=0.9),
+        }[opt]()
+        self._scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
+
+    def __call__(self, loss, params, retain_graph=False):
+        assert len(loss.shape) == 0, loss.shape
+        metrics = {}
+        metrics[f"{self._name}_loss"] = loss.detach().cpu().numpy()
+        self._scaler.scale(loss).backward()
+        self._scaler.unscale_(self._opt)
+        # loss.backward(retain_graph=retain_graph)
+        norm = torch.nn.utils.clip_grad_norm_(params, self._clip)
+        if self._wd:
+            self._apply_weight_decay(params)
+        self._scaler.step(self._opt)
+        self._scaler.update()
+        # self._opt.step()
+        self._opt.zero_grad()
+        metrics[f"{self._name}_grad_norm"] = norm.item()
+        return metrics
+
+    def _apply_weight_decay(self, varibs):
+        nontrivial = self._wd_pattern != r".*"
+        if nontrivial:
+            raise NotImplementedError
+        for var in varibs:
+            var.data = (1 - self._wd) * var.data
+
+
+def args_type(default):
+    def parse_string(x):
+        if default is None:
+            return x
+        if isinstance(default, bool):
+            return bool(["False", "True"].index(x))
+        if isinstance(default, int):
+            return float(x) if ("e" in x or "." in x) else int(x)
+        if isinstance(default, (list, tuple)):
+            return tuple(args_type(default[0])(y) for y in x.split(","))
+        return type(default)(x)
+
+    def parse_object(x):
+        if isinstance(default, (list, tuple)):
+            return tuple(x)
+        return x
+
+    return lambda x: parse_string(x) if isinstance(x, str) else parse_object(x)
+
+
+def static_scan(fn, inputs, start):
+    last = start
+    indices = range(inputs[0].shape[0])
+    flag = True
+    for index in indices:
+        inp = lambda x: (_input[x] for _input in inputs)
+        last = fn(last, *inp(index))
+        if flag:
+            if type(last) == type({}):
+                outputs = {
+                    key: value.clone().unsqueeze(0) for key, value in last.items()
+                }
+            else:
+                outputs = []
+                for _last in last:
+                    if type(_last) == type({}):
+                        outputs.append(
+                            {
+                                key: value.clone().unsqueeze(0)
+                                for key, value in _last.items()
+                            }
+                        )
+                    else:
+                        outputs.append(_last.clone().unsqueeze(0))
+            flag = False
+        else:
+            if type(last) == type({}):
+                for key in last.keys():
+                    outputs[key] = torch.cat(
+                        [outputs[key], last[key].unsqueeze(0)], dim=0
+                    )
+            else:
+                for j in range(len(outputs)):
+                    if type(last[j]) == type({}):
+                        for key in last[j].keys():
+                            outputs[j][key] = torch.cat(
+                                [outputs[j][key], last[j][key].unsqueeze(0)], dim=0
+                            )
+                    else:
+                        outputs[j] = torch.cat(
+                            [outputs[j], last[j].unsqueeze(0)], dim=0
+                        )
+    if type(last) == type({}):
+        outputs = [outputs]
+    return outputs
+
+
+# Original version
+# def static_scan2(fn, inputs, start, reverse=False):
+#  last = start
+#  outputs = [[] for _ in range(len([start] if type(start)==type({}) else start))]
+#  indices = range(inputs[0].shape[0])
+#  if reverse:
+#    indices = reversed(indices)
+#  for index in indices:
+#    inp = lambda x: (_input[x] for _input in inputs)
+#    last = fn(last, *inp(index))
+#    [o.append(l) for o, l in zip(outputs, [last] if type(last)==type({}) else last)]
+#  if reverse:
+#    outputs = [list(reversed(x)) for x in outputs]
+#  res = [[]] * len(outputs)
+#  for i in range(len(outputs)):
+#    if type(outputs[i][0]) == type({}):
+#      _res = {}
+#      for key in outputs[i][0].keys():
+#        _res[key] = []
+#        for j in range(len(outputs[i])):
+#          _res[key].append(outputs[i][j][key])
+#        #_res[key] = torch.stack(_res[key], 0)
+#        _res[key] = faster_stack(_res[key], 0)
+#    else:
+#      _res = outputs[i]
+#      #_res = torch.stack(_res, 0)
+#      _res = faster_stack(_res, 0)
+#    res[i] = _res
+#  return res
