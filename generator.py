4dfeb766ae88d2037365e14e0e6d78922b490ce9
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-08-26 12:07:18 +0000

    Continued refactoring.

--- generator.py
+++ generator.py
@@ -126,3 +126,134 @@ def main(conf,
         # Unroll one episode
 
         epsteps = 0
+        timer = time.time()
+        obs = env.reset()
+        done = False
+        metrics = defaultdict(list)
+
+        while not done:
+            action, mets = policy(obs)
+            obs, reward, done, inf = env.step(action)
+            steps += 1
+            epsteps += 1
+            for k, v in mets.items():
+                metrics[k].append(v)
+
+        episodes += 1
+        data = inf['episode']  # type: ignore
+        if 'policy_value' in metrics:
+            data['policy_value'] = np.array(metrics['policy_value'] + [np.nan])     # last terminal value is null
+            data['policy_entropy'] = np.array(metrics['policy_entropy'] + [np.nan])  # last policy is null
+            data['action_prob'] = np.array([np.nan] + metrics['action_prob'])       # first action is null
+        else:
+            # Need to fill with placeholders, so all batches have the same keys
+            data['policy_value'] = np.full(data['reward'].shape, np.nan)
+            data['policy_entropy'] = np.full(data['reward'].shape, np.nan)
+            data['action_prob'] = np.full(data['reward'].shape, np.nan)
+
+        # Log
+
+        fps = epsteps / (time.time() - timer + 1e-6)
+        print_once('Episode data sample: ', {k: v.shape for k, v in data.items()})
+
+        info(f"Episode recorded:"
+             f"  steps: {epsteps}"
+             f",  reward: {data['reward'].sum():.1f}"
+             f",  terminal: {data['terminal'].sum():.0f}"
+            #  f",  visited: {(data.get('map_seen', np.zeros(1))[-1] > 0).mean():.1%}"
+             f",  total steps: {steps:.0f}"
+             f",  episodes: {episodes}"
+             f",  saved steps (train): {steps_saved:.0f}"
+             f",  fps: {fps:.0f}"
+             )
+
+        if log_mlflow_metrics:
+            metrics = {f'{metrics_prefix}/{k}': np.array(v).mean() for k, v in metrics.items()}
+            all_returns.append(data['reward'].sum())
+            metrics.update({
+                f'{metrics_prefix}/episode_length': epsteps,
+                f'{metrics_prefix}/fps': fps,
+                f'{metrics_prefix}/steps': steps,  # All steps since previous restart
+                f'{metrics_prefix}/steps_saved': steps_saved,  # Steps saved in the training repo
+                f'{metrics_prefix}/env_steps': steps * env_action_repeat,
+                f'{metrics_prefix}/episodes': episodes,
+                f'{metrics_prefix}/return': data['reward'].sum(),
+                f'{metrics_prefix}/return_cum': np.array(all_returns[-100:]).mean(),
+            })
+
+            # Calculate return_discounted
+
+            rewards_v = data['reward'].copy()
+            if not data['terminal'][-1]:
+                avg_value = rewards_v.mean() / (1.0 - metrics_gamma)
+                rewards_v[-1] += avg_value
+            returns_discounted = discount(rewards_v, gamma=metrics_gamma)
+            metrics[f'{metrics_prefix}/return_discounted'] = returns_discounted.mean()
+
+            # Calculate policy_value_terminal
+
+            if data['terminal'][-1]:
+                value_terminal = data['policy_value'][-2] - data['reward'][-1]  # This should be zero, because value[last] = reward[last]
+                metrics[f'{metrics_prefix}/policy_value_terminal'] = value_terminal
+
+            # Goal visibility metrics for Scavenger
+
+            if 'goals_visage' in data:
+                goals_seen = data['goals_visage'] < 1e5
+                metrics[f'{metrics_prefix}/goals_seen_avg'] = goals_seen.sum(axis=-1).mean()
+                metrics[f'{metrics_prefix}/goals_seen_last'] = goals_seen[-1].sum()
+                metrics[f'{metrics_prefix}/goals_seenage'] = (data['goals_visage'] * goals_seen).sum() / goals_seen.sum()
+
+            # Aggregate every 10 episodes
+
+            for k, v in metrics.items():
+                if not np.isnan(v):
+                    metrics_agg[k].append(v)
+
+            if len(metrics_agg[f'{metrics_prefix}/return']) >= log_every:
+                metrics_agg_max = {k: np.array(v).max() for k, v in metrics_agg.items()}
+                metrics_agg = {k: np.array(v).mean() for k, v in metrics_agg.items()}
+                metrics_agg[f'{metrics_prefix}/return_max'] = metrics_agg_max[f'{metrics_prefix}/return']
+                metrics_agg['_timestamp'] = datetime.now().timestamp()
+                mlflow_log_metrics(metrics_agg, step=model_step)
+                metrics_agg = defaultdict(list)
+
+        # Save to npz
+
+        datas.append(data)
+        datas_episodes = len(datas)
+        datas_steps = sum(len(d['reset']) - 1 for d in datas)
+
+        if datas_steps >= steps_per_npz:
+
+            # Concatenate episodes
+
+            data = {}
+            for key in datas[0]:
+                data[key] = np.concatenate([b[key] for b in datas], axis=0)
+            datas = []
+            print_once('Collected data sample: ', {k: v.shape for k, v in data.items()})
+
+            # ... or chunk
+
+            # if steps_per_npz=1000, then chunk size will be [1000,1999]
+            if datas_steps >= 2 * steps_per_npz:
+                chunks = chunk_episode_data(data, steps_per_npz)
+            else:
+                chunks = [data]
+
+            # Save to npz
+
+            repo = repository if (np.random.rand() > split_fraction) else repository2
+            for i, data in enumerate(chunks):
+                if 'image' in data and len(data['image'].shape) == 4:
+                    # THWC => HWCT for better compression
+                    data['image_t'] = data['image'].transpose(1, 2, 3, 0)
+                    del data['image']
+                else:
+                    # Categorical image, leave it alone
+                    pass
+                repo.save_data(data, episodes - datas_episodes, episodes - 1, i)
+
+            if repo == repository:
+                # Only count steps in the training repo, so that prefill and limit_step_ratio works correctly
