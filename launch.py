c8c27bdd9ca064786c7999224589a90de69f6520
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-08-28 22:56:31 +0000

    Add version

--- launch.py
+++ launch.py
@@ -12,4 +12,8 @@ import generator
 import train
 # from pydreamer.models import dreamer
 from pydreamer.tools import (configure_logging, mlflow_log_params,mlflow_log_text,
-                      
+                             mlflow_init, print_once, read_yamls)
+from pydreamer import envs
+from pydreamer.envs.__init__ import create_env
+
+os.environ
