b74e97f51b134003bc4ceb9fee8aa1248b1d1b3d
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-10-24 22:20:18 +0000

    multiplayer

--- pydreamer/envs/atari.py
+++ pydreamer/envs/atari.py
@@ -11,4 +11,7 @@ class Atari_v2(gym.Env):
     LOCK = threading.Lock()
 
     def __init__(self,
- 
+                 name,
+                 action_repeat=4,
+                 size=(64, 64),
+                 grayscale=False,  # DreamerV2 uses grayscale=Tr
