e45299937d1cd2a12a87b7bd5a6735d822672b99
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-12-04 21:47:00 +0000

    Clean and consolidate export code #190

--- pydreamer/envs/dmc.py
+++ pydreamer/envs/dmc.py
@@ -10,4 +10,7 @@ class DMC_v2(gym.Env):
     def __init__(self, name, action_repeat=1, size=(64, 64), camera=None):
         # os.environ['MUJOCO_GL'] = 'egl'
         domain, task = name.split('_', 1)
-        if domain == 'cup':  # Only d
+        if domain == 'cup':  # Only domain with multiple words.
+            domain = 'ball_in_cup'
+        if domain == 'manip':
+            from dm_control import manipulatio
