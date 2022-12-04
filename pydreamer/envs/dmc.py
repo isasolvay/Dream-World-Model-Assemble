c4a46cac1067db1648edd4929579d92acddd6456
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-12-04 09:10:11 +0000

    updating CHANGELOG and bumping metadata

--- pydreamer/envs/dmc.py
+++ pydreamer/envs/dmc.py
@@ -8,4 +8,6 @@ import numpy as np
 class DMC_v2(gym.Env):
 
     def __init__(self, name, action_repeat=1, size=(64, 64), camera=None):
-     
+        # os.environ['MUJOCO_GL'] = 'egl'
+        domain, task = name.split('_', 1)
+        if domain == 'cup':  # Only d
