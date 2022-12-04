6f5bfa13946cd0e29395566d3cb7cc1ba3af946b
isasolvay <142871848+isasolvay@users.noreply.github.com>
2022-12-04 07:09:35 +0000

    - converted neededforbuild to BuildRequires

--- pydreamer/envs/dmc.py
+++ pydreamer/envs/dmc.py
@@ -1,4 +1,11 @@
 import os
 
 import gym
-impo
+import gym.spaces
+import numpy as np
+
+
+class DMC_v2(gym.Env):
+
+    def __init__(self, name, action_repeat=1, size=(64, 64), camera=None):
+     
