133107228881dce34a18ccf1f7a5fe777670a48e
isasolvay <142871848+isasolvay@users.noreply.github.com>
2023-01-15 16:44:03 +0000

    Fix test cases

--- pydreamer/envs/dmlab.py
+++ pydreamer/envs/dmlab.py
@@ -56,4 +56,6 @@ ACTION_SET = {  # R2D2 action set
 ALL_GAMES = frozenset([
     'rooms_collect_good_objects_train',  # rooms_collect_good_objects
     'rooms_collect_good_objects_test',  # rooms_collect_good_objects
-    'rooms_exploit_deferred_effects_t
+    'rooms_exploit_deferred_effects_train',  # rooms_exploit_deferred_effects
+    'rooms_exploit_deferred_effects_test',  # rooms_exploit_deferred_effects
+   
