171afe496a76012b2e25835595b46cb4db632b60
isasolvay <142871848+isasolvay@users.noreply.github.com>
2023-02-19 15:11:18 +0000

    fix return

--- pydreamer/envs/embodied.py
+++ pydreamer/envs/embodied.py
@@ -3,4 +3,11 @@ from typing import List
 
 import gym
 import gym.spaces
-from pydreamer.tools import prin
+from pydreamer.tools import print_once
+
+import embodied
+from embodied.envs import load_single_env
+
+
+class EmbodiedEnv(gym.Env):
+    """gym.Env wrapper around embo
