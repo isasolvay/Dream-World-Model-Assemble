e10009a78b5cfbcd38de93381999a2d967c29d8e
isasolvay <142871848+isasolvay@users.noreply.github.com>
2023-03-12 15:44:57 +0000

    Code optimization with multiple processes and zeromq interprocess communication library

--- pydreamer/envs/minigrid.py
+++ pydreamer/envs/minigrid.py
@@ -2,4 +2,7 @@ from typing import Tuple
 
 import gym
 import gym.spaces
-impor
+import gym_minigrid
+import gym_minigrid.envs
+import gym_minigrid.minigrid
+from gym_minigrid.minigrid import COLOR_TO_IDX, OBJECT_TO_IDX
