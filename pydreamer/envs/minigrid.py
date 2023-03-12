5b88f259b9ad476affce3ae132b159f2591b8b37
isasolvay <142871848+isasolvay@users.noreply.github.com>
2023-03-12 19:39:06 +0000

    Prepare for next developments

--- pydreamer/envs/minigrid.py
+++ pydreamer/envs/minigrid.py
@@ -6,3 +6,10 @@ import gym_minigrid
 import gym_minigrid.envs
 import gym_minigrid.minigrid
 from gym_minigrid.minigrid import COLOR_TO_IDX, OBJECT_TO_IDX
+import numpy as np
+
+
+class MiniGrid(gym.Env):
+
+    GRID_VALUES = np.array([  # shape=(33,3)
+        [0, 0, 0],  # Invisible
