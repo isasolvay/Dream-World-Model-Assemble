f698b0ba506878e227d36de5dd6b1a3dd94665cf
isasolvay <142871848+isasolvay@users.noreply.github.com>
2023-04-01 07:29:07 +0000

    add formula

--- pydreamer/envs/minigrid.py
+++ pydreamer/envs/minigrid.py
@@ -117,4 +117,5 @@ class MiniGrid(gym.Env):
         obs['map_vis'] = self.update_map_last_seen(vis_mask)
         obs['map_centered'] = self.to_categorical(self.map_centered())
         obs['agent_pos'] = np.array(self.env.agent_pos, dtype=np.float32)
-        agent_dir = int(s
+        agent_dir = int(self.env.agent_dir)  # type: ignore
+        obs['agent_dir'] = np.array([[+1, 0], [0, +1], [-1, 0], [0, -1]][agent_dir], dtype=np.flo
