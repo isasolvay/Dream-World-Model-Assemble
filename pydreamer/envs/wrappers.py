bd49a958ecaa6863444b722703a8d1ad1aae046b
isasolvay <142871848+isasolvay@users.noreply.github.com>
2023-08-04 11:24:43 +0000

    Only apps should have lockfiles

--- pydreamer/envs/wrappers.py
+++ pydreamer/envs/wrappers.py
@@ -185,4 +185,6 @@ class NormalizeActions(gym.Wrapper):
         self._high = np.where(self._mask, env.action_space.high, 1)
 
     def action_space(self):
-        low = np.
+        low = np.where(self._mask, -np.ones_like(self._low), self._low)
+        high = np.where(self._mask, np.ones_like(self._low), self._high)
+        r
