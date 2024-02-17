1fc4df2ced0fdff9266c5a42c26f7282434fae95
isasolvay <142871848+isasolvay@users.noreply.github.com>
2024-02-17 20:48:20 +0000

    Store git-svn files as of .

--- pydreamer/models/networks/encoders.py
+++ pydreamer/models/networks/encoders.py
@@ -104,3 +104,137 @@ class MultiEncoder_v2(nn.Module):
         if self.encoder_image:
             image = obs['image']
             T, B, C, H, W = image.shape
+            if self.reward_input:
+                reward = obs['reward']
+                terminal = obs['terminal']
+                reward_plane = reward.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand((T, B, 1, H, W))
+                terminal_plane = terminal.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand((T, B, 1, H, W))
+                image = torch.cat([image,  # (T,B,C+2,H,W)
+                                reward_plane.to(image.dtype),
+                                terminal_plane.to(image.dtype)], dim=-3)
+
+            embed_image = self.encoder_image.forward(image)  # (T,B,E)
+            embeds.append(embed_image)
+
+        if self.encoder_vecobs:
+            embed_vecobs = self.encoder_vecobs(obs['vecobs'])
+            embeds.append(embed_vecobs)
+
+        embed = torch.cat(embeds, dim=-1)  # (T,B,E+256)
+        return embed
+    
+class MultiEncoder_v3(nn.Module):
+    def __init__(
+        self,
+        shapes,
+        conf,
+    ):
+        super(MultiEncoder_v3, self).__init__()
+        self.wm_type=conf.wm_type
+        
+        
+        excluded = ("reset", "is_last", "terminal", "reward")
+        shapes = {
+            k: v
+            for k, v in shapes.items()
+            if k not in excluded and not k.startswith("log_")
+        }
+        self.cnn_shapes = {
+            k: v for k, v in shapes.items() if len(v) == 3 and re.match(conf.encoder["cnn_keys"], k)
+        }
+        self.mlp_shapes = {
+            k: v
+            for k, v in shapes.items()
+            if len(v) in (1, 2) and re.match(conf.encoder["mlp_keys"], k)
+        }
+        print("Encoder CNN shapes:", self.cnn_shapes)
+        print("Encoder MLP shapes:", self.mlp_shapes)
+
+        self.out_dim = 0
+        if self.cnn_shapes:
+            input_ch = sum([v[-1] for v in self.cnn_shapes.values()])
+            input_shape = tuple(self.cnn_shapes.values())[0][:2] + (input_ch,)
+            self._cnn = ConvEncoder(
+                input_shape, self.wm_type,conf.encoder["cnn_depth"], conf.encoder["act"], conf.encoder["norm"], conf.encoder["kernel_size"], 
+                conf.encoder["minres"]
+            )
+            self.out_dim += self._cnn.out_dim
+        if self.mlp_shapes:
+            input_size = sum([sum(v) for v in self.mlp_shapes.values()])
+            self._mlp = MLP_v3(
+                input_size,
+                None,
+               conf.encoder["mlp_layers"],
+                conf.encoder["mlp_units"],
+                conf.encoder["act"],
+                conf.encoder["norm"],
+                symlog_inputs=conf.encoder["symlog_inputs"],
+            )
+            self.out_dim += conf.encoder["mlp_units"]
+
+    def forward(self, obs):
+        outputs = []
+        if self.cnn_shapes:
+            inputs = torch.cat([obs[k] for k in self.cnn_shapes], -1)
+            outputs.append(self._cnn(inputs))
+        if self.mlp_shapes:
+            inputs = torch.cat([obs[k] for k in self.mlp_shapes], -1)
+            outputs.append(self._mlp(inputs))
+        outputs = torch.cat(outputs, -1)
+        return outputs
+
+
+class ConvEncoder(nn.Module):
+
+    def __init__(self, input_shape, wm_type,cnn_depth=32, act="SiLU",norm="LayerNorm",minres=4,kernel_size=4):
+        super().__init__()
+        self.wm_type=wm_type
+        # if wm_type=="v2":
+        h,w,input_ch= input_shape
+        activation=nn.ELU
+        self.out_dim = cnn_depth * 32
+        
+        stride = 2
+        #中间层的channer数
+        d = cnn_depth
+        self.layers = nn.Sequential(
+            nn.Conv2d(input_ch, d, kernel_size, stride),
+            activation(),
+            nn.Conv2d(d, d * 2, kernel_size, stride),
+            activation(),
+            nn.Conv2d(d * 2, d * 4, kernel_size, stride),
+            activation(),
+            nn.Conv2d(d * 4, d * 8, kernel_size, stride),
+            activation(),
+            nn.Flatten()
+        )
+        # elif wm_type=="v3":
+        #     h, w, input_ch = input_shape
+        #     act = getattr(torch.nn, act)
+        #     norm = getattr(torch.nn, norm)
+        #     layers = []
+        #     for i in range(int(np.log2(h) - np.log2(minres))):
+        #         if i == 0:
+        #             in_dim = input_ch
+        #         else:
+        #             in_dim = 2 ** (i - 1) * cnn_depth
+        #         out_dim = 2**i * cnn_depth
+        #         layers.append(
+        #             Conv2dSame(
+        #                 in_channels=in_dim,
+        #                 out_channels=out_dim,
+        #                 kernel_size=kernel_size,
+        #                 stride=2,
+        #                 bias=False,
+        #             )
+        #         )
+        #         layers.append(ChLayerNorm(out_dim))
+        #         layers.append(act())
+        #         h, w = h // 2, w // 2
+
+            # self.out_dim = out_dim * h * w
+            # self.layers = nn.Sequential(*layers)
+            # self.layers.apply(tools_v3.weight_init)
+
+    def forward(self, x):
+        # if self.wm_type=='v2':
