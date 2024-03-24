5b344c62051a37fa8d19c30d2de69660234b067d
isasolvay <142871848+isasolvay@users.noreply.github.com>
2024-03-24 20:22:48 +0000

    department

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ extras = {}
 setuptools.setup(
     name='pydreamer',
     version='0.1.0',
-    packages=['pyd
+    packages=['pydreamer', 'pydreamer.models', 'pydreamer.envs'],
+    author='Jurgis Pašukonis',
+    author_email='jurgisp@g
