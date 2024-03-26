19ef6cc6c3be83960ed7c476aafc75e1d26d72f4
isasolvay <142871848+isasolvay@users.noreply.github.com>
2024-03-26 10:53:42 +0000

    Synced with mainline.

--- setup.py
+++ setup.py
@@ -8,4 +8,7 @@ setuptools.setup(
     version='0.1.0',
     packages=['pydreamer', 'pydreamer.models', 'pydreamer.envs'],
     author='Jurgis Pašukonis',
-    author_email='jurgisp@g
+    author_email='jurgisp@gmail.com',
+    description='DreamerV2 in PyTorch',
+    url='http://github.com/jurgisp/pydreamer',
+    long_description=pathlib.Path('README.md').read_tex
