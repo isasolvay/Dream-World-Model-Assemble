8cafe3d3a43897b0aedcb8926dc1a99e82e88583
isasolvay <142871848+isasolvay@users.noreply.github.com>
2024-04-11 19:50:58 +0000

    removing junk

--- useful_metric.py
+++ useful_metric.py
@@ -4,4 +4,5 @@ from pathlib import Path
 import os
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="argumen
+    parser = argparse.ArgumentParser(description="argument parser", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--uri", default='99f26e5dc334468ba20cbaa433
