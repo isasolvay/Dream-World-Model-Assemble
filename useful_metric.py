618ae0eb8275d0e187179ccffb3c9b5939df90f0
isasolvay <142871848+isasolvay@users.noreply.github.com>
2024-04-14 19:08:45 +0000

    Making tests pass

--- useful_metric.py
+++ useful_metric.py
@@ -7,4 +7,9 @@ def parse_args():
     parser = argparse.ArgumentParser(description="argument parser", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--uri", default='99f26e5dc334468ba20cbaa43361a1e9', type=str)
     parser.add_argument("--tidy", default=False, type=bool,required=False)
-    parser.add_argument("-
+    parser.add_argument("--index", default='0', type=str)
+    parser.add_argument("--env", default='atari_pong', type=str)
+    return parser.parse_args()
+
+
+def plo
