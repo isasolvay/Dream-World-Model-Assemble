3637abde04a43d207075b6e670deb8e9ee7a4c5c
isasolvay <142871848+isasolvay@users.noreply.github.com>
2024-04-14 09:42:42 +0000

    Updating translations for locale/fr/BOINC-Manager.po [skip ci]

--- useful_metric.py
+++ useful_metric.py
@@ -5,4 +5,6 @@ import os
 
 def parse_args():
     parser = argparse.ArgumentParser(description="argument parser", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--uri", default='99f26e5dc334468ba20cbaa433
+    parser.add_argument("--uri", default='99f26e5dc334468ba20cbaa43361a1e9', type=str)
+    parser.add_argument("--tidy", default=False, type=bool,required=False)
+    parser.add_argument("-
